7434ec6068dec638e6a5a2c0c8565495044dc7b4
AnthonyTsu1984 <wenjia.tu@zilliz.com>
2023-04-25 20:30:15 +0800

    Update commit.py
    Signed-off-by: AnthonyTsu1984 <wenjia.tu@zilliz.com>

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,5 +1,15 @@
 import json
 
+def del_examples(body):
+    for prop in body['properties']:
+        if 'examples' in body['properties'][prop]:
+            del body['properties'][prop]['examples']
+        if 'properties' in body['properties'][prop]:
+            del_examples(body['properties'][prop])
+
+    print('examples' in body)
+
+
 if __name__ == '__main__':
 
     with open('apis/openapi.json', 'r') as f:
@@ -12,6 +22,31 @@ if __name__ == '__main__':
                     for param in specifications['paths'][url][method]['parameters']:
                         if 'example' in param:
                             del param['example']
+
+                if 'requestBody' in specifications['paths'][url][method]:
+                    del specifications['paths'][url][method]['requestBody']['content']['application/json']['example']
+                    schema = specifications['paths'][url][method]['requestBody']['content']['application/json']['schema']
+                    if 'anyOf' in schema:
+                        for req_body in schema['anyOf']:
+                            del_examples(req_body)
+                    else:
+                        del_examples(schema)
+
+                if 'responses' in specifications['paths'][url][method]:
+                    res_body = specifications['paths'][url][method]['responses']['200']['content']['application/json']['schema']
+                    if 'anyOf' in specifications['paths'][url][method]['responses']['200']['content']['application/json']['schema']:
+                        schemas = specifications['paths'][url][method]['responses']['200']['content']['application/json']['schema']['anyOf']
+                        res_body = [ x for x in schemas if 'data' in x['properties'] ][0]
+                    else:
+                        res_body = specifications['paths'][url][method]['responses']['200']['content']['application/json']['schema']
+                    
+                    del_examples(res_body)
+
+        del specifications['components']
+
+        with open('apis/servers.json', 'r') as f:
+            servers = json.load(f)
+            specifications['servers'] = servers['servers']
 
         with open('apis/clean.json', 'w') as f:
             json.dump(specifications, f, indent=4)
