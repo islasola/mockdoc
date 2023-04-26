7b6b801589047a8dc7a55512ba7448909f1c2a1c
AnthonyTsu1984 <wenjia.tu@zilliz.com>
2023-04-26 09:21:14 +0800

    Update scripts
    Signed-off-by: AnthonyTsu1984 <wenjia.tu@zilliz.com>

--- scripts/refgen.py
+++ scripts/refgen.py
@@ -30,7 +30,7 @@ def req_format(req_body):
                 if 'properties' in v['items']:
                     for k2,v2 in v['items']['properties'].items():
                         b2[0][k2] = v2['type']
-                    b[k] = b2
+                b[k] = b2
             else:
                 b[k] = v['type']
     elif 'items' in req_body:
@@ -40,8 +40,6 @@ def req_format(req_body):
             b = [{}]
             for k,v in properties.items():
                 b[0][k] = v['type']
-        elif items['type'] == 'object':
-            b = [{}]
 
     return json.dumps(b, indent=4, sort_keys=True)
       
