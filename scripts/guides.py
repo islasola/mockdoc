9290658328df7d7f1f61d108a65a87d0e98fe332
AnthonyTsu1984 <wenjia.tu@zilliz.com>
2023-06-03 15:52:54 +0800

    Update guides.py
    Signed-off-by: AnthonyTsu1984 <wenjia.tu@zilliz.com>

--- scripts/guides.py
+++ scripts/guides.py
@@ -157,7 +157,7 @@ async def faqs(category):
 
     faqs = await notion_client.post(f"/v1/databases/{FAQS_DATABASE_ID}/query", json=query)
 
-    print(faqs['results'])
+    print(json.dumps(faqs, indent=2))
 
     faqs_categories = list(set([ x['properties']['Category']['multi_select'][0]['name'] for x in faqs['results'] ]))
     faqs_questions = [ dict(category=x['properties']['Category']['multi_select'][0]['name'], question=x['properties']['Question']['title'][0]['plain_text']) for x in faqs['results'] ]
